854bf95870dfb55778c14db086360488b874bd6c
Mahmud <ajaomahmud@gmail.com>
2022-08-05 00:44:06 +0100

    chore: Modify module path

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ProductsModule } from './modules/products/products.module';
-import { CategoriesModule } from './categories/categories.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './core/database/database.module';
 
