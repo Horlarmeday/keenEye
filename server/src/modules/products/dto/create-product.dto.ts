2f38c5ac186e31580bd5f2da61681bc35de517b3
Mahmud <ajaomahmud@gmail.com>
2022-08-05 00:46:51 +0100

    feat: Add product dto

--- server/src/modules/products/dto/create-product.dto.ts
+++ server/src/modules/products/dto/create-product.dto.ts
@@ -1,1 +1,30 @@
-export class CreateProductDto {}
+import {IsNotEmpty, IsNumber, IsOptional, IsString, IsUUID} from 'class-validator';
+import {Type} from "class-transformer";
+import {IsFile} from "../../../core/decorators/file.decorators";
+// import { IsFile } from '../../../core/decorators/file.decorators';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly name: string;
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  readonly price: number;
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  readonly minimum_quantity: number;
+
+  @IsNotEmpty()
+  @Type(() => Number)
+  readonly discount_rate: number;
+
+  @IsNotEmpty()
+  @IsUUID()
+  readonly category_id: string;
+  //
+  // @IsFile({ mime: ['image/jpg', 'image/png'] })
+  @IsOptional()
+  readonly image?: any;
+}
--- server/src/modules/users/dto/create-user.dto.ts
+++ server/src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,27 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { Provider } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly username: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  readonly email?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly password?: string;
+
+  @IsOptional()
+  @IsEnum(Provider)
+  readonly provider?: Provider;
+}
