b514cdbb4a5d6da24bd127d4d21434d8095eef9a
Mahmud <ajaomahmud@gmail.com>
2022-08-05 00:47:20 +0100

    feat: Add local auth guard

--- server/src/core/guards/doesUserExist.guards.ts
+++ server/src/core/guards/doesUserExist.guards.ts
@@ -1,20 +1,31 @@
-import { CanActivate, ExecutionContext, Injectable, ForbiddenException } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  ForbiddenException,
+} from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { UsersService } from '../../modules/users/users.service';
+import { Messages } from '../responses/message.responses';
 
 @Injectable()
 export class DoesUserExist implements CanActivate {
   constructor(private readonly usersService: UsersService) {}
 
-  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
     return this.validateRequest(request);
   }
 
   async validateRequest(request) {
-    const userExist = await this.usersService.findOneByEmailOrUsername(request.body.email, request.body.username);
+    const userExist = await this.usersService.findOneByEmailOrUsername(
+      request.body.email,
+      request.body.username,
+    );
     if (userExist) {
-      throw new ForbiddenException('Account already exist');
+      throw new ForbiddenException(Messages.USER_EXISTS);
     }
     return true;
   }
