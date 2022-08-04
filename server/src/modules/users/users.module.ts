4699912660a73f5d1418ec9a88363bce7d3dd75e
Mahmud <ajaomahmud@gmail.com>
2022-08-05 00:51:37 +0100

    feat: Add users module

--- server/src/modules/users/users.module.ts
+++ server/src/modules/users/users.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  controllers: [UsersController],
-  providers: [UsersService]
+  imports: [SequelizeModule.forFeature([User])],
+  providers: [UsersService],
+  exports: [UsersService, SequelizeModule],
 })
 export class UsersModule {}
--- server/src/modules/users/users.service.ts
+++ server/src/modules/users/users.service.ts
@@ -1,26 +1,91 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './entities/user.entity';
+import { Messages } from '../../core/responses/message.responses';
+import { Op } from 'sequelize';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(User) private usersRepository: typeof User) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    const { password } = createUserDto;
+    return await this.usersRepository.create<User>({
+      ...createUserDto,
+      last_login_date: new Date(),
+      password: password ? await this.hashPassword(password) : null,
+    });
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async hashPassword(password: string) {
+    const salt = await bcrypt.genSalt(12);
+    return await bcrypt.hash(password, salt);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOneByEmailOrUsername(
+    email: string,
+    username: string,
+  ): Promise<User> {
+    return await this.usersRepository.findOne<User>({
+      where: {
+        [Op.or]: [
+          {
+            email: email || '',
+          },
+          {
+            username: username || '',
+          },
+        ],
+      },
+    });
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findOneById(id: string): Promise<User> {
+    const user = await this.usersRepository.findOne<User>({
+      where: { id },
+      attributes: { exclude: ['password'] },
+    });
+    if (!user) {
+      throw new NotFoundException(Messages.USER_NOT_FOUND);
+    }
+    return user;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findOneByEmail(email: string): Promise<User> {
+    return await this.usersRepository.findOne<User>({ where: { email } });
+  }
+
+  async saveLastLoginDate(userId: string): Promise<unknown> {
+    return this.usersRepository.update<User>(
+      { last_login_date: new Date() },
+      { where: { id: userId } },
+    );
+  }
+
+  async setCurrentRefreshToken(refreshToken: string, userId: string) {
+    const currentHashedRefreshToken = await bcrypt.hash(refreshToken, 10);
+    await this.usersRepository.update<User>(
+      { currentHashedRefreshToken },
+      {
+        where: {
+          id: userId,
+        },
+      },
+    );
+  }
+
+  async getUserIfRefreshTokenMatches(refreshToken: string, userId: string) {
+    const user = await this.findOneById(userId);
+
+    const isRefreshTokenMatching = await bcrypt.compare(
+      refreshToken,
+      user.currentHashedRefreshToken,
+    );
+
+    if (isRefreshTokenMatching) {
+      return user;
+    }
   }
 }
